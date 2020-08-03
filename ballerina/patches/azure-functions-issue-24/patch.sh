681e3f951a69ea94654e3236a6a8f46be734e035
lafernando <lafernando@gmail.com>
2020-08-03 09:58:30 -0700

    Update

---
+++ ballerina/patches/azure-functions-issue-24/patch.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+BAL_HOME=`ballerina home`
+sudo cp azure.functions.bir $BAL_HOME/bir-cache/ballerinax/azure.functions/0.0.0/
+sudo cp ballerinax-azure.functions-0.0.0.jar $BAL_HOME/bre/lib/
+sudo cp azurefunctions-extension-1.2.6.jar $BAL_HOME/bre/lib/azurefunctions-extension-1.2.6.jar
+
